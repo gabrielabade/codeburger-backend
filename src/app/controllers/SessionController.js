092a60843ee0f8dfa36ac26a3ac23da24c1ce9d8
gabrielabade <bade.gabriela@gmail.com>
2024-02-28 19:39:44 -0300

    fix: re-up session wrong deleted

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,43 @@
+import * as Yup from "yup"
+import jwt from "jsonwebtoken"
+import authConfig from "../../config/auth.js"
+import User from "../models/User.js"
+
+class SessionController {
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    })
+
+    const userEmailOrPasswordIncorrect = () => {
+      return response
+        .status(400)
+        .json({ error: "Make sure your email or password are correct" })
+    }
+
+    if (!(await schema.isValid(request.body)))
+      return userEmailOrPasswordIncorrect()
+
+    const { email, password } = request.body
+
+    const user = await User.findOne({
+      where: { email },
+    })
+
+    if (!user) return userEmailOrPasswordIncorrect()
+    if (!(await user.checkPassword(password)))
+      return userEmailOrPasswordIncorrect()
+
+    return response.json({
+      id: user.id,
+      email,
+      name: user.name,
+      admin: user.admin,
+      token: jwt.sign({ id: user.id, name: user.name }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    })
+  }
+}
+export default new SessionController()
